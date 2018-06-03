f0aaa97b378ce461f1f96d52b27149512b1dc4d9
Marco <mondial95@gmail.com>
2018-06-03 16:40:44 +0200

    ✨ 🎨 dashboard access control, templates template refactoring

--- app/controllers/dashboard.php
+++ app/controllers/dashboard.php
@@ -1,3 +1,11 @@
 <?php
 
+/**
+ * Serve dashboard to logged users only
+ */
+if (!USER_LOGGED) {
+  // redirect to home
+  header('Location: ../');
+  exit;
+}
 include VIEWS_DIR . '/dashboard/index.html';
