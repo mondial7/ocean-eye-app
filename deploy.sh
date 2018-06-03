647b5a62dc9293cfb762259584f67b76e41930e7
Marco <mondial95@gmail.com>
2018-06-03 16:32:34 +0200

    🐙 🎨 refactoring build/deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,7 @@
-# TODO empty logs
 # TODO update configs -> DB credentials
 
-cd ..
-rm -r build
-# create deployment directory
-mkdir build
-# build frontend repositories
-cd ocean-eye-landing/
-polymer build --preset 'es5-bundled' --base-path '/ocean-eye/app/assets/landing/'
-cd ../ocean-eye/
-polymer build --preset 'es5-bundled' --base-path '/ocean-eye/app/assets/dashboard/'
-cd ..
-echo '🔥🔥🔥 Frontend ready'
-# copy php shell app in build
-cp -r ocean-eye-app/app/ build/app/
-cp ocean-eye-app/.htaccess build/
-cp ocean-eye-app/index.php build/
-# include built frontend repos
-cp -r ocean-eye-landing/build/es5-bundled/ build/app/assets/landing/
-cp -r ocean-eye/build/es5-bundled/ build/app/assets/dashboard/
-mv build/app/assets/landing/index.html build/app/templates/landing/index.html
-mv build/app/assets/dashboard/index.html build/app/templates/dashboard/index.html
-# clean logs folder
-rm -rf build/app/core/logs/
-
-echo '🔥🔥🔥 Done building'
+# prepare build
+bash build.sh
 
 # start deployment
 echo '🚀🚀🚀 Ready to deploy!'
@@ -33,4 +10,5 @@ cd build
 # echo 'put -rp ./ ./ocean-eye/' > deploy_rules # need a ssh key to use this
 sftp startupradiator.com@ssh.startupradiator.com
 
-echo '🌈🍻🍷 https://startupradiator.com/ocean-eye/'
+echo '🌈🍻🍷'
+echo '      https://startupradiator.com/ocean-eye/'
--- local_build.sh
+++ local_build.sh
@@ -8,6 +8,7 @@ polymer build --preset 'es5-bundled' --base-path '/uni/radiator/build/app/assets
 cd ../ocean-eye/
 polymer build --preset 'es5-bundled' --base-path '/uni/radiator/build/app/assets/dashboard/'
 cd ..
+echo '🔥🔥🔥 Frontend ready'
 # copy php shell app in build
 cp -r ocean-eye-app/app/ build/app/
 cp ocean-eye-app/.htaccess build/
@@ -17,5 +18,6 @@ cp -r ocean-eye-landing/build/es5-bundled/ build/app/assets/landing/
 cp -r ocean-eye/build/es5-bundled/ build/app/assets/dashboard/
 mv build/app/assets/landing/index.html build/app/templates/landing/index.html
 mv build/app/assets/dashboard/index.html build/app/templates/dashboard/index.html
-echo 'Done building... 🛠'
-echo 'Application available at //localhost:8888/uni/radiator/build/'
+echo '🔥🔥🔥 Done building'
+echo 'Application available at'
+echo '      localhost:8888/uni/radiator/build/'
